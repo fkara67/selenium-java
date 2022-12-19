dccbc4b7b2a321f55023f3a532bd86cd2de4b897
Fatih Kara <fkara1667@gmail.com>
2022-12-19 17:01:55 +0300

    demo

--- selenium.project/src/Demo1.java
+++ selenium.project/src/Demo1.java
@@ -3,7 +3,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demo1 {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","C:/Users/fkara/Downloads/chromedriver/chromedriver.exe");
+        // this line is optional since Selenium version 4.6.0
+        //System.setProperty("webdriver.chrome.driver","C:/Users/fkara/Downloads/chromedriver/chromedriver.exe");
         WebDriver drive = new ChromeDriver();
         drive.get("https://github.com/fkara67");
         String str = drive.getCurrentUrl();
