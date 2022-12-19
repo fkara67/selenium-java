c0d07cbb283e3707ea3528d7ab6808bbccc9cae6
Fatih Kara <fkara1667@gmail.com>
2022-12-19 16:16:24 +0300

    demo

---
+++ selenium.project/src/Demo1.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:/Users/fkara/Downloads/chromedriver/chromedriver.exe");
+        WebDriver drive = new ChromeDriver();
+        drive.get("https://github.com/fkara67");
+        String str = drive.getCurrentUrl();
+        System.out.println("Test 1: " + (str));
+        // String pageSource = drive.getPageSource();
+        //System.out.println(pageSource);
+    }
+}
+
