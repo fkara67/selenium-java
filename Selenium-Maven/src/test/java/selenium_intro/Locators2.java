0caf609ac967ac0875f364c5b01c531a258e449b
Fatih Kara <fkara1667@gmail.com>
2022-12-23 23:50:28 +0300

    seleniumIntro

--- Selenium-Maven/src/test/java/selenium_intro/Locators2.java
+++ Selenium-Maven/src/test/java/selenium_intro/Locators2.java
@@ -4,8 +4,9 @@ package selenium_intro;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import java.time.Duration;
-//import org.testng.Assert;
+
 
 public class Locators2 {
     public static void main(String[] args) throws InterruptedException {
@@ -21,11 +22,11 @@ public class Locators2 {
         driver.findElement(By.className("signInBtn")).click();
         Thread.sleep(1000);
         System.out.println(driver.findElement(By.tagName("p")).getText());
-        //Assert.assertEquals(driver.findElement
-          //      (By.tagName("p")).getText(), "You are successfully logged in.");
+        Assert.assertEquals(driver.findElement
+                (By.tagName("p")).getText(), "You are successfully logged in.");
 
-        //Assert.assertEquals(driver.findElement
-          //      (By.cssSelector("div[class='login-container'] h2")).getText(),"Hello "+name+",");
+        Assert.assertEquals(driver.findElement
+               (By.cssSelector("div[class='login-container'] h2")).getText(),"Hello "+name+",");
         driver.findElement(By.xpath("//button[text()='Log Out']")).click();
         driver.close();
 
