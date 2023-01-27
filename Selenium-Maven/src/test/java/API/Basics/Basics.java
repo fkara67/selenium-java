dffcfcb24e092fcb16f5f431503f229cc18f290e
Fatih Kara <fkara1667@gmail.com>
2023-01-27 12:58:57 +0300

    APITestingBasics

--- Selenium-Maven/src/test/java/API/Basics/Basics.java
+++ Selenium-Maven/src/test/java/API/Basics/Basics.java
@@ -1,8 +1,10 @@
 package API.Basics;
 
+import API.Files.Payloads;
 import io.restassured.RestAssured;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class Basics {
     public static void main(String[] args) {
@@ -16,23 +18,11 @@ public class Basics {
 
         RestAssured.baseURI = "https://rahulshettyacademy.com";
         given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
-                .body("{\n" +
-                        "\"location\": {\n" +
-                        "\"lat\": -38.383494,\n" +
-                        "\"lng\": 33.427362\n" +
-                        "},\n" +
-                        "\"accuracy\": 50,\n" +
-                        "\n" +
-                        "\"name\":\"Fatih Kara\",\n" +
-                        "\"phone_number\": \"(+90) 546 114 6716\",\n" +
-                        "\"address\": \"29, side layout, cohen 09\",\n" +
-                        "\"types\": [\n" +
-                        "\"shoe park\",\n" +
-                        "\"shop\"\n" +
-                        "],\n" +
-                        "\"website\": \"http://google.com\",\n" +
-                        "\"language\": \"French-IN\"\n" +
-                        "}").when().post("maps/api/place/add/json")
-                .then().log().all().assertThat().statusCode(200);
+                .body(Payloads.addPlace()).when().post("maps/api/place/add/json")
+                .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
+                .header("Server", "Apache/2.4.41 (Ubuntu)");
+
+        // Add Place -> update place with new address -> get place to validate if new address is present in response
+
     }
 }
