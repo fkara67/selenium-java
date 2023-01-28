c804d2d9cb100eb4e3eab022a0ddea7941019d7a
Fatih Kara <fkara1667@gmail.com>
2023-01-28 23:17:47 +0300

    APITestingBasics

---
+++ Selenium-Maven/src/test/java/API/HandlingDynamicJson/DynamicJson.java
@@ -0,0 +1,12 @@
+package API.HandlingDynamicJson;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class DynamicJson {
+
+    @Test
+    public void addBook() {
+        RestAssured.baseURI = "http://216.10.245.166";
+    }
+}
